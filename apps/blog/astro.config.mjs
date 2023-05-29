7d56f17fbcb6fb6f20d35c2b1a3dc2ee6da25beb
inglkruiz <12603425+inglkruiz@users.noreply.github.com>
2023-05-29 13:05:10 +0200

    chore(blog): add deploy command
    Signed-off-by: inglkruiz <12603425+inglkruiz@users.noreply.github.com>

--- apps/blog/astro.config.mjs
+++ apps/blog/astro.config.mjs
@@ -4,6 +4,10 @@ import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: 'https://inglkruiz.github.io',
+  build: {
+    assets: 'assets',
+  },
   markdown: {
     shikiConfig: {
       theme: 'dracula',
